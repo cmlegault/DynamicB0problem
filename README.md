be79c5c0099ea804876f940016ac005c0d05b8e3
cmlegault <chris.legault@noaa.gov>
2018-12-12 14:57:42 -0500

    add files and description

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # DynamicB0problem
-demonstrates the problem of dynamic B0 dependency on SR curve 
+Demonstrates the problem of dynamic B0 dependency on SR curve .
+
+## What is dynamic B0?
+Simply an estimate of what would have happened to a fish population if no fishing had occurred. Computationally, it is found by starting in the first year of the stock assessment with the estimated population and rolling it forward with just natural mortality. The "trick" is that the recruitment is generated based on the stock recruitment curve and the estimated recruitment deviation. This means that whatever caused the recruitment to be above or below the curve would have happened under no fishing independent of the size of the spawning stock biomass. The calculations also assume no changes in the weights at age or maturity at age under no fishing compared to what was observed historically.
+
+The advantage of dynamic B0 is that is allows direct evaluation of the effect of fishing on the population that accounts for the deviations of recruitment from the stock recruitment relationship. This is also its downside though as it depends on a good estimate of the stock recruitment relationship. 
+
+## What was done?
+A recent Atlantic herring stock assessment found no ability to estimate a stock recruitment relationship. This assessment was used to demonstrate the dependency of dynamic B0 on the stock recruitment relationship. The stock assessment was run in ASAP with four different fixed values of steepness: 0.4, 0.6, 0.8, and 1.0. The four assessments were nearly identical, but the estimated dynamic B0 and associated depletion (current SSB divided by the current dynamic B0) varied widely, demonstrating the dependence of the approach on the ability to estimate the stock recruitment relationship.
+
+## Into the weeds!
+
+### Dynamic B0 calculation in ASAP
+A function was written to use the rdat file output by ASAP to calculate dynamic B0. The function first creates a blank matrix for the population abundance at age and year along with a blank vector for the spawning stock biomass - the dynamic B0 values. The first year in the matrix is filled with the estimated numbers at age from ASAP and the spawning stock biomass (SSB) in the first year computed assuming only natural mortality occurs between the start of the year and the time of spawning. The SSB values use the year-specific weights at age for SSB and maturity at age from the rdat file as well.
+
+The expected recruitment in the following year is calculated from the Beverton and Holt stock recruitment curve estimates of alpha and beta in the rdat file as E(R) = alpha * SSB / (beta + SSB). The recruitment at age 1 in the following year is then created by multiplying the expected recruitment by exponentiating the log-scale recruitment residual: R = E(R) * exp(log_resid) estimated for that year. This means that whatever environmental effects caused recruitment to deviate from the curve would occur again under this no fishing situation. The remainder of the ages in the following year are filled using the exponential decline assocaited with natural moratality: N(y+1,a+1) = N(y,a) * exp(-M(y,a)), with the plus group additionally accounting for the survivors from the previous plus group. The SSB is calculated for this year and used to estimate recruitemnt in the following year. This process continues through all the years estimated in the rdat file. The function then returns the time series of spawning stock biomass under the assumption of no fishing (dynamic B0).
+
+### What happened?
+As mentioned above, the four stock assessments were nearly identical. They all had the same objective function value (3773.103), the SSB varied by a maximum of 0.1 metric tons (mean SSB 310,000 metric tons), and the age-1 recruitment estimates varied by a maximum of 1,000 fish (mean recruitment 4.1 billion fish). However, due to fixing the steepness at different values, the estimated R0 varied widely:
+| Steepness | R0 (thousands) |
+| --------- | -------------- |
+| 0.4       | 14,493,710     |
+| 0.6       |  4,669,791     |
+| 0.8       |  3,621,096     |
+| 1.0       |  3,193,192     |
+
+The stock recruitment curves all pass through the data reasonably well.
+![SR plot](srplot.png)
+
+Computing the dynamic B0 resulted in large differences among the four steepness values both in absolute and relative scales.
+![SSB plot](ssbplot.png)
+![relative SSB plot](relssbplot.png)
+
+The current depeletion was estimated as the SSB in the final year from the original assessment divided by the dynamic B0 value in the final year, and varied widely:
+| Steepness | Current Depletion |
+| --------- | ----------------- |
+| 0.4       |   0.055   |
+| 0.6       |   0.198   |
+| 0.8       |   0.266   |
+| 1.0       |   0.303   |
+
+## Conclusion
+The dynamic B0 approach depends on the ability to estimate the stock recruitment relationship correctly. In situations where this is not possible, such as the Atlantic herring stock assessment, the approach should not be used.
---
+++ calcDynamicB0.R
@@ -0,0 +1,125 @@
+# set working directory to Source File Location
+
+library(ggplot2)
+
+calcDynamicB0 <- function(asap){
+  nyears <- asap$parms$nyears
+  nages <- asap$parms$nages
+  
+  NAA <- matrix(NA, nrow = nyears, ncol = nages)
+  dynB0 <- rep(NA, nyears)
+  
+  NAA[1, ] <- asap$N.age[1, ]
+  dynB0[1] <- sum(NAA[1, ] * exp(-asap$options$frac.yr.spawn * asap$M.age[1, ]) * 
+                    asap$maturity[1, ] * asap$WAA.mats$WAA.ssb[1, ])
+  
+  for (iy in 2:nyears){
+    erec <- asap$SR.parms$SR.alpha * dynB0[iy-1] / (asap$SR.parms$SR.beta + dynB0[iy-1])
+    NAA[iy] <- erec * exp(asap$SR.resids$logR.dev[iy])
+    for (ia in 2:nages){
+      NAA[iy, ia] <- NAA[iy-1, ia-1] * exp(-asap$M.age[iy-1, ia-1])
+    }
+    NAA[iy, nages] <- NAA[iy, nages] + NAA[iy-1, nages] * exp(-asap$M.age[iy-1, nages])
+    dynB0[iy] <- sum(NAA[iy, ] * exp(-asap$options$frac.yr.spawn * asap$M.age[iy, ]) * 
+                       asap$maturity[iy, ] * asap$WAA.mats$WAA.ssb[iy, ])
+    
+  }
+  return(dynB0)
+}
+
+# steepness values used
+mysteep <- c(0.4, 0.6, 0.8, 1.0)
+
+# get the four ASAP runs, only difference among them is the value steepness fixed at
+asap04 <- dget("steep04.rdat")
+asap06 <- dget("steep06.rdat")
+asap08 <- dget("steep08.rdat")
+asap10 <- dget("steep10.rdat")
+
+# they have the same objective function value, SSB within 0.1 mt, and R within 1000 fish
+c(asap04$like$lk.total, asap06$like$lk.total, asap08$like$lk.total, asap10$like$lk.total)
+max(abs(asap04$SSB - asap10$SSB))
+max(abs(asap06$SSB - asap10$SSB))
+max(abs(asap08$SSB - asap10$SSB))
+max(abs(asap04$N.age[, 1] - asap10$N.age[, 1]))
+max(abs(asap06$N.age[, 1] - asap10$N.age[, 1]))
+max(abs(asap08$N.age[, 1] - asap10$N.age[, 1]))
+
+# but R0 differs widely
+cbind(mysteep, c(asap04$SR.parms$SR.R0, asap06$SR.parms$SR.R0, asap08$SR.parms$SR.R0, asap10$SR.parms$SR.R0))
+
+# make SR plot showing the four fits
+nyears <- asap04$parms$nyears
+srdatdf <- data.frame(SSB = asap10$SSB[1:(nyears-1)],
+                      Recruits = asap10$N.age[2:nyears, 1])
+npoints <- 1000
+ssb04 <- seq(0, asap04$SR.parms$SR.S0, length.out = npoints)
+r04 <- asap04$SR.parms$SR.alpha * ssb04 / (asap04$SR.parms$SR.beta + ssb04)
+ssb06 <- seq(0, asap06$SR.parms$SR.S0, length.out = npoints)
+r06 <- asap06$SR.parms$SR.alpha * ssb06 / (asap06$SR.parms$SR.beta + ssb06)
+ssb08 <- seq(0, asap08$SR.parms$SR.S0, length.out = npoints)
+r08 <- asap08$SR.parms$SR.alpha * ssb08 / (asap08$SR.parms$SR.beta + ssb08)
+ssb10 <- seq(0, asap10$SR.parms$SR.S0, length.out = npoints)
+r10 <- asap10$SR.parms$SR.alpha * ssb10 / (asap10$SR.parms$SR.beta + ssb10)
+r10[1] <- 0 # by definition, replaces calculated NaN
+
+srdf <- data.frame(steep = as.factor(rep(mysteep, each = npoints)),
+                   SSB = c(ssb04, ssb06, ssb08, ssb10),
+                   Recruits = c(r04, r06, r08, r10))
+
+srplot <- ggplot(srdf, aes(x=SSB, y=Recruits)) +
+  geom_line(aes(group=steep, color=steep)) +
+  geom_point(data=srdatdf, aes(x=SSB, y=Recruits)) +
+  theme_bw()
+print(srplot)
+ggsave("srplot.png")
+
+# calculate dynamic B0 for each run
+steep04B0 <- calcDynamicB0(asap04)
+steep06B0 <- calcDynamicB0(asap06)
+steep08B0 <- calcDynamicB0(asap08)
+steep10B0 <- calcDynamicB0(asap10)
+
+# make data frame for plotting
+nyears <- asap04$parms$nyears
+years <- seq(asap04$parms$styr, asap04$parms$endyr)
+sdf <- data.frame(Year = rep(years, 4),
+                  steep = as.factor(rep(mysteep, each=nyears)),
+                  dynB0 = c(steep04B0, steep06B0, steep08B0, steep10B0),
+                  SSB = c(asap04$SSB, asap06$SSB, asap08$SSB, asap10$SSB),
+                  reldynB0 = c(steep04B0 / steep04B0[1],
+                               steep06B0 / steep06B0[1],
+                               steep08B0 / steep08B0[1],
+                               steep10B0 / steep10B0[1]),
+                  relSSB = c(asap04$SSB / asap04$SSB[1],
+                             asap06$SSB / asap06$SSB[1],
+                             asap08$SSB / asap08$SSB[1],
+                             asap10$SSB / asap10$SSB[1]))
+
+ssbplot <- ggplot(sdf, aes(x=Year, y=dynB0, group=steep, color=steep)) +
+  geom_line() +
+  geom_line(aes(x=Year, y=SSB, group=steep, color=steep), size=1.5) +
+  facet_wrap(steep~., scales = "free_y") +
+  ylab("SSB") +
+  theme_bw()
+print(ssbplot)
+ggsave("ssbplot.png")
+
+relssbplot <- ggplot(sdf, aes(x=Year, y=reldynB0, group=steep, color=steep)) +
+  geom_line() +
+  geom_line(aes(x=Year, y=relSSB, group=steep, color=steep), size=1.5) +
+  facet_wrap(steep~., scales = "free_y") +
+  ylab("Relative SSB") +
+  theme_bw()
+print(relssbplot)
+ggsave("relssbplot.png")
+
+# get most recent year depletion using dynamic B0 approach
+curr04 <- asap04$SSB[nyears] / steep04B0[nyears]
+curr06 <- asap04$SSB[nyears] / steep06B0[nyears]
+curr08 <- asap04$SSB[nyears] / steep08B0[nyears]
+curr10 <- asap04$SSB[nyears] / steep10B0[nyears]
+
+mytab <- cbind(mysteep, round(c(curr04, curr06, curr08, curr10), 3))
+colnames(mytab) <- c("Steepness", "Current Depletion")
+mytab
